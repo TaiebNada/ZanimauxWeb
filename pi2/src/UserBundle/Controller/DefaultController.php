11a43a864d4802b5a3a9fa648f0a32cb7e06c750
TaiebNada <38006932+TaiebNada@users.noreply.github.com>
2018-05-22 15:38:15 +0100

    é
    x

---
+++ pi2/src/UserBundle/Controller/DefaultController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace UserBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class DefaultController extends Controller
+{
+    public function indexAction()
+    {
+        return $this->render('@User/Default/index.html.twig');
+    }
+
+
+
+}
